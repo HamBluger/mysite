a7c456ad9a464c95a8ab06e9378553e8de5d7297
HamBluger <yahia.external@gmail.com>
2024-08-15 14:51:06 +0300

    Add files via upload

---
+++ list.py
@@ -0,0 +1,58 @@
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
+a = 2
+a2 = 2
+import time
+checked = True
+
+print("From 0 to 29")
+def check_func(list_1, checked = True):
+    for i in range(1):
+        i=-2
+        if i > list_1[7]:
+            print(list_1[-1])
+            if checked == True:
+                checked = False
+                print("Var CHECKED Set to ", checked)
+        i=-1 
+    if checked == False:
+        checked = True
+        print("Var CHECKED Set to ", checked)
+def list_board(cft):
+    list_checkfunc = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    crt = check_func(list_checkfunc)
+    cgt = print(crt, cft)
+    ckt = print(cgt)
+    return ckt
+    return crt
+    return cft
+    return cgt
+def board_func(list_2):
+    list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    if list_2[28] > 20:
+        print(0)
+        for i in range(29):
+            list_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]]
+            print(" [ ", list_2[int(input("\n [ Start of index - : ")):int(input("\n [ End of index - : "))], check_func([1, 2, 3, 4, 5, 6, 7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, [1, 2, 3, 4, 5, 6, 7, list_2]]), " ] ")
+            check_func(list_2)
+            check_func([1, 2, 3, 4, 5, 6, 7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+            time.sleep(0.5)
+            list_board(list_2)
+def last_board(list_9, checked):
+    list_9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+    list_9[30] = input("\n [y]es / [n]o [ Start? - : ")
+    if list_9[30] == "y":
+        print(0)
+        for i in range(1):
+            list_9 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, [1, 2, 3, 4, 5, 6, 7]] and print(check_func(list_1), print(board_func(list_1)))
+            board_func(list_9)
+            
+    if list_9[30] == "n":
+        for i in range(31):
+            time.sleep(0.3)
+            print(i + 0.3)
+        print(counter)
+
+        print("\n [ MESSAGE FROM CODE ] : Fine... I'll exit... ;(")
+            
+
+last_board(list_1, checked)
---
+++ list_1.py
@@ -0,0 +1,9 @@
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+def maximum_minimum(my_list):
+    max_num=max(my_list)
+    min_num=min(my_list)
+    print("\n List:", "[ ",my_list," ]")
+    print("\n Biggest Number is", "[ ",min_num," ]")
+    print("\n Smallest number is", "[ ",min_num," ]")
+    print()
+maximum_minimum(list_1)
